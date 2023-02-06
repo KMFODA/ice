e09d0cc52501902228f1ca653b5c1fd507f0ac6f
Karim Foda <35491698+KMFODA@users.noreply.github.com>
2023-02-06 14:53:52 +0000

    Update squad.py

--- ice/agents/squad.py
+++ ice/agents/squad.py
@@ -1,20 +1,20 @@
-from anyio.to_thread import run_sync
-from transformers.pipelines import pipeline
+#from anyio.to_thread import run_sync
+#from transformers.pipelines import pipeline
 
-from ice.agents.base import Agent
+#from ice.agents.base import Agent
 
 
-class SquadAgent(Agent):
-    def __init__(self, model_name: str = "z-uo/roberta-qasper"):
+#class SquadAgent(Agent):
+#    def __init__(self, model_name: str = "z-uo/roberta-qasper"):
 
-        self.nlp = pipeline(
-            "question-answering", model=model_name, tokenizer=model_name
-        )
+#        self.nlp = pipeline(
+#            "question-answering", model=model_name, tokenizer=model_name
+#        )
 
-    async def relevance(
-        self, *, question, context, verbose=False, default=None
-    ) -> float:
-        response = await run_sync(
-            self.nlp, dict(question=question, context=context), cancellable=True
-        )
-        return response["score"]
+#    async def relevance(
+#        self, *, question, context, verbose=False, default=None
+#    ) -> float:
+#        response = await run_sync(
+#            self.nlp, dict(question=question, context=context), cancellable=True
+#        )
+#        return response["score"]
